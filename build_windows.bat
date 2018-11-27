54a8777436e022599b314d43498986353ee97ca4
NeatNit <NeatNit@gmail.com>
2018-11-27 13:55:18 +0200

    fixes in build and test script

--- build_windows.bat
+++ build_windows.bat
@@ -16,65 +16,64 @@ REM Paramater %4: InOut only - stdout output file
 
 REM Start compilation
 REM =================
-ECHO Compiling %1 to %~n1.exe ...
+ECHO Compiling %1 to "%~n1.exe" ...
 
-gcc -O -Wall -Wextra -Werror -Wpedantic -std=c99 %1 -o %~n1.exe
+gcc -O -Wall -Wextra -Werror -Wpedantic -std=c99 %1 -o "%~n1.exe"
 
 REM Check for compilation errors
 REM ============================
 IF %ERRORLEVEL% NEQ 0 (
-    ECHO.
+    ECHO,
     ECHO Compilation failed.
-    ECHO.
+    ECHO,
     EXIT
 )
 
 ECHO Compilation finished successfully.
-ECHO.
+ECHO,
 
 
 REM Run appropriate execution method
 REM ================================
 IF /I [%2] EQU [Empty] (
     ECHO Running program with no stdin...
-    %~n1.exe < NUL
+    "%~n1.exe" < NUL
 
 ) ELSE IF /I [%2] EQU [Interactive] (
     ECHO Running program interactively in new window...
-    START /WAIT CMD /c "%~n1.exe & PAUSE"
+    START /WAIT CMD /c "^"%~n1.exe^" & PAUSE"
     ECHO Finished.
 
 ) ELSE IF /I [%2] EQU [Test] (
     ECHO Testing program...
-    ECHO.
+    ECHO,
 
     SET _passed=0
     SET _failed=0
 
     REM Find test files:
-    FOR /R %%T IN (%~n1in*.txt) DO (
-        SET _input=%%T
+    FOR /R %%T IN ("%~n1in*.txt") DO (
+        SET _input="%%T"
         REM Find corresponding expected output file by replacing in with out:
         SET _expected=!_input:%~n1in=%~n1out!
 
 
-        REM To do: Check that _expected exists, and run the tests including comparison with FC
-        ECHO.
+        ECHO,
 
         
 
         ECHO Input - !_input!:
         TYPE !_input!
 
-        ECHO.
-        ECHO.
+        ECHO,
+        ECHO,
         ECHO Output:
 
-        %~n1.exe < !_input! > test_out.txt
+        "%~n1.exe" < !_input! > test_out.txt
         TYPE test_out.txt
 
-        ECHO.
-        ECHO.
+        ECHO,
+        ECHO,
         IF EXIST !_expected! (
             REM Compare expected output and actual output
             FC /B !_expected! test_out.txt > NUL
@@ -84,17 +83,17 @@ IF /I [%2] EQU [Empty] (
             ) ELSE (
                 REM There is a difference.
                 SET _failed=1
-                ECHO.
-                ECHO.
+                ECHO,
+                ECHO,
                 ECHO TEST FAILED - output doesn't match^^!
                 FC /N !_expected! test_out.txt
                 IF !ERRORLEVEL! EQU 0 ECHO There is a missing or extra newline character ^(\n^) at the end of the file^^!
             )
 
         ) ELSE ECHO Expected output file not found: !_expected!
-        ECHO.
+        ECHO,
         ECHO ------------
-        ECHO.
+        ECHO,
     )
     IF !_failed! EQU 1 (
         IF !_passed! EQU 0 (ECHO ALL TESTS FAILED.) ELSE (ECHO SOME TESTS FAILED.)
@@ -102,11 +101,4 @@ IF /I [%2] EQU [Empty] (
         IF !_passed! EQU 1 (ECHO ALL TESTS PASSED.) ELSE (ECHO NO TESTS FOUND.)
     )
 
-) ELSE IF /I [%2] EQU [In]  (
-    ECHO stdin - %3:
-    TYPE %3
-    ECHO.
-    ECHO.
-    ECHO Running program...
-    REM %~n1.exe ^< %3
 )
